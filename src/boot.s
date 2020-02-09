bcbb422e2bf7a449ecbc4ce82a6a4b07a8b58055
Abbasov Ceyhun <abbasovceyhunn@hotmail.com>
2020-02-09 14:53:50 +0400

    modified

--- src/boot.s
+++
@@ -1,9 +0,0 @@
-entry start
-start:
-      mov ax,#0xb800
-      mov es,ax
-      seg es
-      mov [0],#0x41
-      seg es
-      mov [1],#0xf9 ;f(white) - background, 9(lightblue) - foreground 
-loop1: jmp loop1
--- src/write.c
+++ src/write.c
@@ -4,19 +4,28 @@
 
 int main()
 {
-        char boot_buf[512];
-        int floppy_desc, file_desc;
- 
-        
-        file_desc = open("./bin/boot", O_RDONLY);
-        read(file_desc, boot_buf, 510);
-        close(file_desc);
-        
-        boot_buf[510] = 0x55;
-        boot_buf[511] = 0xaa;
+                char boot_buf[512];
+                int floppy_desc, file_desc;
 
-        floppy_desc = open("../floppy.img", O_RDWR);
-        lseek(floppy_desc, 0, SEEK_CUR);
-        write(floppy_desc, boot_buf, 512);
-        close(floppy_desc);
+                file_desc = open("./bin/bootsector", O_RDONLY);
+
+                read(file_desc, boot_buf, 510);
+                close(file_desc);
+
+                boot_buf[510] = 0x55;
+                boot_buf[511] = 0xaa;
+
+                floppy_desc = open("../floppy.img", O_RDWR);
+
+                lseek(floppy_desc, 0, SEEK_SET);
+                write(floppy_desc, boot_buf, 512);
+
+                file_desc = open("./bin/sector2", O_RDONLY);
+                read(file_desc, boot_buf, 512);
+                close(file_desc);
+
+                lseek(floppy_desc, 512, SEEK_SET);
+                write(floppy_desc, boot_buf, 512);
+
+                close(floppy_desc);
 }
